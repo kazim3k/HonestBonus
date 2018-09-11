7c3d73ef976be6c93285cd36941c9ea280f3b697
norbertx30 <norbertx30@gmail.com>
2018-09-11 22:25:01 +0200

    add project

---
+++ src/test/java/com/github/groupproject/UserDto/UserDtoTest.java
@@ -0,0 +1,19 @@
+package com.github.groupproject.UserDto;
+
+import com.github.groupproject.dto.UserDto;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UserDtoTest {
+
+//    @Test
+//    public String getUuidtest() {
+//
+//
+//
+//    }
+
+
+}
+
+
